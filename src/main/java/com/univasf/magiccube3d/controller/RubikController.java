3338743a856b610ed2df3d8ccdc82878166ec2cf
4ndersu <anderson.vicente@discente.univasf.edu.br>
2025-05-23 15:21:00 -0300

    Mostrando face do cubo que está virado para a câmera(corretamente VERSÃO FINAL)

--- src/main/java/com/univasf/magiccube3d/controller/RubikController.java
+++ src/main/java/com/univasf/magiccube3d/controller/RubikController.java
@@ -366,10 +366,10 @@ public class RubikController {
         } else if ((ay >= 225 && ay <= 315)) {
             return "RIGHT";
         } else if ((ay >= 135 && ay <= 225)) {  // inclui limites para BACK
-            return "BACK";
+            return "FRONT";
         } else {
             // Se não entrou em nenhum dos casos acima, é FRONT (vermelho)
-            return "FRONT";
+            return "BACK";
         }
     }
 
